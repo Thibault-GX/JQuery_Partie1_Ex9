d5e0f27805cd6ad2c2faab5c1114f01a1d2477a3
Thibault-GX <thibault.grimoux@gmail.com>
2019-11-05 11:01:39 +0100

    CA FONCTIONNE ! Mais push initial, aussi.

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+$(function(){
+  $("#firstText , #thirdText").css('border','5px dashed green'); //Attention à l'ordre des caractéristiques de la bordure !
+  // $("#thirdText").css('border','5px dashed green');
+});
